613ee57acd1996eb0dcbbc269077daa286ed9c41
ChoDoGyu <doing9498@gmail.com>
2026-09-19 19:47:34 +0900

    chore: Quest 패키지 개발 환경 및 기본 구조 구성

---
+++ com.chodogyu.quest/Runtime/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CDG.Quest.Editor")]
+[assembly: InternalsVisibleTo("CDG.Quest.Tests.Runtime")]
+[assembly: InternalsVisibleTo("CDG.Quest.Tests.Editor")]
